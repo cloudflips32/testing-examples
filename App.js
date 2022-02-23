f8aeb73c6ca7b935b54f331fdb88d860ff1d5a11
cloudflips32 <64573965+cloudflips32@users.noreply.github.com>
2022-02-23 13:22:34 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,19 +1,6 @@
 import React, { Component } from "react";
-import Split from "./composition/Split";
 import './App.css'
-import Tooltip from './composition/Tooltip'
 import Messages from './Messages';
-
-const firstTooltip = (
-  <Tooltip color='hotpink' message='tooltip message'>
-   ipsum
-  </Tooltip>
-)
-const secondTooltip = (
-  <Tooltip color='#126BCC' message='another tooltip message'>
-    officiis
-  </Tooltip>
-)
 
 class App extends Component {
   render() {
